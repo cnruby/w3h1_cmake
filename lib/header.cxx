58811e26e2d64e4d3bbc65e3f67adb8b9f59126c
cnruby <gudao.luo@gmail.com>
2020-04-16 18:57:17 +0000

    add 'format code'

--- lib/header.cxx
+++ lib/header.cxx
@@ -1,5 +1,6 @@
-#include <iostream>
 #include "header.hxx"
+
+#include <iostream>
 
 void Header::hello(const char* name) {
   std::cout << "Hello, Static Library from " << name << "!" << std::endl;
