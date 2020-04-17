074b58dda72813accf8b0df8f4c1299971165cb4
cnruby <gudao.luo@gmail.com>
2020-04-17 13:20:46 +0000

    add 'format-code'

--- lib/impl/header.cxx
+++ lib/impl/header.cxx
@@ -2,9 +2,8 @@
 
 #include <iostream>
 
-Header::Header(const char* _name){
-  name = _name;
-}
+Header::Header(const char* _name) { name = _name; }
 void Header::hello() {
-  std::cout << "Hello, Library from Different Source " << name << "!" << std::endl;
+  std::cout << "Hello, Library from Different Source " << name << "!"
+            << std::endl;
 }
