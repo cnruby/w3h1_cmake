b801abee3b308be15561dbc2d33c7100c97a50a2
cnruby <gudao.luo@gmail.com>
2020-04-19 22:15:22 +0000

    add 'format-code'

--- config/config.hxx
+++ config/config.hxx
@@ -6,11 +6,14 @@
 
 #define PROJECT_NAME "basic_116"
 #define PROJECT_SOURCE_DIR "/Users/Simon/Documents/dev/cpp-ws/116_basic"
-#define CMAKE_CURRENT_SOURCE_DIR "/Users/Simon/Documents/dev/cpp-ws/116_basic/cmake"
-#define CMAKE_RUNTIME_OUTPUT_DIRECTORY "/Users/Simon/Documents/dev/cpp-ws/116_basic/bin"
+#define CMAKE_CURRENT_SOURCE_DIR \
+  "/Users/Simon/Documents/dev/cpp-ws/116_basic/cmake"
+#define CMAKE_RUNTIME_OUTPUT_DIRECTORY \
+  "/Users/Simon/Documents/dev/cpp-ws/116_basic/bin"
 
-//const char *CXX_VARIABLE_NAME = ""
-const char *PROJECT_CONIFG_DIR = "/Users/Simon/Documents/dev/cpp-ws/116_basic/config/";
+// const char *CXX_VARIABLE_NAME = ""
+const char *PROJECT_CONIFG_DIR =
+    "/Users/Simon/Documents/dev/cpp-ws/116_basic/config/";
 
 #endif
 //
--- src/main.cxx
+++ src/main.cxx
@@ -8,9 +8,11 @@ int main(int, char**) {
 
   std::cout << "PROJECT_NAME \t\t\t= " << PROJECT_NAME << std::endl;
   std::cout << "PROJECT_SOURCE_DIR \t\t= " << PROJECT_SOURCE_DIR << std::endl;
-  std::cout << "CMAKE_CURRENT_SOURCE_DIR \t= " << CMAKE_CURRENT_SOURCE_DIR << std::endl;
-  std::cout << "CMAKE_RUNTIME_OUTPUT_DIRECTORY \t= " << CMAKE_RUNTIME_OUTPUT_DIRECTORY << std::endl;
-  
+  std::cout << "CMAKE_CURRENT_SOURCE_DIR \t= " << CMAKE_CURRENT_SOURCE_DIR
+            << std::endl;
+  std::cout << "CMAKE_RUNTIME_OUTPUT_DIRECTORY \t= "
+            << CMAKE_RUNTIME_OUTPUT_DIRECTORY << std::endl;
+
   std::cout << "PROJECT_CONIFG_DIR \t\t= " << PROJECT_CONIFG_DIR << std::endl;
 
   return 0;
