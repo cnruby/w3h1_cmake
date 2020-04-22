2d13842412ff7141ebd670336d90f6c8726419a2
Gudao Luo <gudao.luo@gmail.com>
2020-04-22 20:09:36 +0200

    add 'tmp'

---
+++ format-codes.rb
@@ -0,0 +1,13 @@
+#
+# The File format-codes.rb
+# ruby format-codes.rb
+#EXTENSIONS = %w[cxx cpp hpp cu c h]
+EXTENSIONS = %w[cxx hxx]
+EXTENSIONS.each do |ext|
+    puts "ext = #{ext}"
+    `clang-format -style=file -i $(find . -name "*.#{ext}")`
+end
+
+`cmake-format -c .cmake-format.yml -i $(find . -name "CMakeLists.txt")`
+`cmake-format -c .cmake-format.yml -i $(find . -name "*.cmake")`
+#
