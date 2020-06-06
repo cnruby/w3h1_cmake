abff56899782345299cabb858c4d478e15f26ca2
Gudao Luo <gudao.luo@gmail.com>
2020-06-06 22:44:53 +0200

    finish "basic_139"

---
+++ docs/cmd/on_project.sh
@@ -0,0 +1,6 @@
+# to install executable to Project for Developer 
+cmake -GNinja -H. -B_build
+cmake -DCMAKE_BUILD_TYPE=Debug --build _build
+ninja -C _build
+ninja -C _build install
+./_install/Debug/bin/main_139
