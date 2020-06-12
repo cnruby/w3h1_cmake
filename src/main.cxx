327b6aa3ed3362adb25374cf1da617a392463386
Gudao Luo <gudao.luo@gmail.com>
2020-06-12 15:42:26 +0200

    finish "basic_141"

--- src/main.cxx
+++ src/main.cxx
@@ -4,6 +4,8 @@
 int main(int, char**) {
   std::cout << "Project ID:\t\t\t" << PROJECT_NAME << std::endl;
   std::cout << "Project Code:\t\t\t" << PROJECT_DESCRIPTION << std::endl;
+  
+  std::cout << "Project Build Type:\t\t" << DEFAULT_BUILD_TYPE << std::endl;
 
   return EXIT_SUCCESS;
 }
