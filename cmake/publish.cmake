5961003b2e38719055d795858002a148273f7261
cnruby <gudao.luo@gmail.com>
2020-06-17 14:33:26 +0000

    add v0.143.36

--- cmake/publish.cmake
+++ cmake/publish.cmake
@@ -7,7 +7,21 @@
 #
 set(_GHR_VERSION 0.13.0)
 set(_GHR_APP ghr_v${_GHR_VERSION}_linux_amd64 )
-set(_SHASUMS v${PROJECT_VERSION}_SHASUMS)
+set(_SHASUMS ${_APP_NAME}_SHASUMS)
+
+add_custom_target(
+  create_artifacts
+  ${CMAKE_COMMAND} -E make_directory ${_APP_OUTPUT_DIRECTORY}
+  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+)
+add_custom_target(
+  zip_app
+  ${CMAKE_COMMAND} -E chdir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} tar -zcvf ${_APP_NAME} ${_MAIN_EXE}
+  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${_APP_NAME} ${_APP_OUTPUT_DIRECTORY}
+  COMMAND ${CMAKE_COMMAND} -E rm ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${_APP_NAME}
+  WORKING_DIRECTORY ${_APP_OUTPUT_DIRECTORY}
+  DEPENDS create_artifacts
+)
 
 # upload the release to Github Assets
 add_custom_target(
@@ -16,7 +30,7 @@ add_custom_target(
   COMMAND
     ${_TOOL_INPUT_DIRECTORY}/${_GHR_APP}/ghr -n ${PROJECT_NAME} v${PROJECT_VERSION} ${_APP_OUTPUT_DIRECTORY}/${_SHASUMS}
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  DEPENDS get_shasums
+  DEPENDS get_shasums zip_app
 )
 
 add_custom_target(
@@ -25,11 +39,12 @@ add_custom_target(
   COMMAND
     ${_TOOL_INPUT_DIRECTORY}/${_GHR_APP}/ghr -t ${AUTH_TOKEN} -n ${PROJECT_NAME} v${PROJECT_VERSION} ${_APP_OUTPUT_DIRECTORY}/${_SHASUMS}
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  DEPENDS get_shasums
+  DEPENDS get_shasums zip_app
 )
 
 add_custom_target(
   get_shasums
+  #${CMAKE_COMMAND} -E sha1sum ${_APP_NAME}>${_SHASUMS}
   bash -c "sha1sum ${_APP_NAME}>${_SHASUMS}"
   WORKING_DIRECTORY ${_APP_OUTPUT_DIRECTORY}
 )
